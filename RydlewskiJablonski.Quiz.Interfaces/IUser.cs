c8d7a0e07464c5d2b0006ed035c6c1dc9b930f5b
Marcin Jabłoński <jablon1994@gmail.com>
2016-06-17 17:10:24 +0200

    Added initial User interface

---
+++ RydlewskiJablonski.Quiz.Interfaces/IUser.cs
@@ -0,0 +1,14 @@
+﻿using RydlewskiJablonski.Quiz.Core;
+
+namespace RydlewskiJablonski.Quiz.Interfaces
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+        UserTypeEnum UserType { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Login { get; set; }
+        string Password { get; set; }
+    }
+}
