462dc40d9cfd3571d1ebc6732d2f26643b347361
Marcin Jabłoński <jablon1994@gmail.com>
2016-06-17 17:19:20 +0200

    Sample test structure

---
+++ RydlewskiJablonski.Quiz.Interfaces/IAnswer.cs
@@ -0,0 +1,8 @@
+﻿namespace RydlewskiJablonski.Quiz.Interfaces
+{
+    public interface IAnswer
+    {
+        string Text { get; set; }
+        bool IsCorrect { get; set; }
+    }
+}
---
+++ RydlewskiJablonski.Quiz.Interfaces/IQuestion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RydlewskiJablonski.Quiz.Interfaces
+{
+    public interface IQuestion
+    {
+        string Text { get; set; }
+        int Points { get; set; }
+        string ImagePath { get; set; }
+        List<IAnswer> Answers { get; set; }
+    }
+}
