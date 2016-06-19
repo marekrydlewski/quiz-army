3302e9a58c0ec4e354f79de7e06e41ba05fd5c5f
Marek Rydlewski <marek.rydlewski@outlook.com>
2016-06-19 21:25:44 +0200

    Add userlistviewmodel

---
+++ RydlewskiJablonski.Quiz.UI/ViewModels/UserListViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using RydlewskiJablonski.Quiz.Interfaces;
+using RydlewskiJablonski.Quiz.UI.Annotations;
+
+namespace RydlewskiJablonski.Quiz.UI.ViewModels
+{
+    class UsersViewModel: INotifyPropertyChanged
+    {
+        private IDAO _dao;
+
+        public UsersViewModel()
+        {
+            _dao = new DAO.DAO();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public List<IUser> GetUsers()
+        {
+            return _dao.GetUsers();
+        }
+    }
+}
