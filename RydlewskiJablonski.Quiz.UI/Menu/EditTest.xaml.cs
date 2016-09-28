2fa4840300790131ae62682d7d1bd30f8bb1ee54
Marek Rydlewski <marek.rydlewski@hotmail.com>
2016-09-28 22:51:25 +0200

    Enable edit test view

--- RydlewskiJablonski.Quiz.UI/Menu/EditTest.xaml.cs
+++ RydlewskiJablonski.Quiz.UI/Menu/EditTest.xaml.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using RydlewskiJablonski.Quiz.Interfaces;
+using RydlewskiJablonski.Quiz.UI.ViewModels;
 
 namespace RydlewskiJablonski.Quiz.UI.Menu
 {
     /// <summary>
     /// Interaction logic for EditTest.xaml
     /// </summary>
-    public partial class EditTest : UserControl
+    public partial class EditTest : UserControl, ISwitchable
     {
         public EditTest()
         {
             InitializeComponent();
         }
+
+        #region ISwitchable Members
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
