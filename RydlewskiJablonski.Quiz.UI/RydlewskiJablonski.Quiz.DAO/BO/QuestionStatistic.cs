8b2ed59fccd480ae8dc433da24da9bed5fa88d1f
Marcin Jabłoński <jablon1994@gmail.com>
2016-09-29 19:14:29 +0200

    EF annotations

--- RydlewskiJablonski.Quiz.UI/RydlewskiJablonski.Quiz.DAO/BO/QuestionStatistic.cs
+++ RydlewskiJablonski.Quiz.UI/RydlewskiJablonski.Quiz.DAO/BO/QuestionStatistic.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using RydlewskiJablonski.Quiz.Interfaces;
 
 namespace RydlewskiJablonski.Quiz.DAO.BO
 {
     public class QuestionStatistic : IQuestionStatistic
     {
+        [Key]
+        [Column(Order = 1)]
         public Guid TestTakeId { get; set; }
         public int TestId { get; set; }
+        [Key]
+        [Column(Order = 2)]
         public int QuestionId { get; set; }
         public double Time { get; set; }
         public double Points { get; set; }
