42b119e477123b7371251c0aff47df5f8a08c9e7
Marcin Jabłoński <jablon1994@gmail.com>
2016-10-02 12:38:20 +0200

    Test stats selection view

---
+++ RydlewskiJablonski.Quiz.UI/ViewModels/StatiscticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using RydlewskiJablonski.Quiz.Interfaces;
+
+namespace RydlewskiJablonski.Quiz.UI.ViewModels
+{
+    public class StatiscticsViewModel
+    {
+        
+    }
+}
